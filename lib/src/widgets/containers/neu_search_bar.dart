b343ec1036f77289e0e0320607c8fad7533a039f
Deepraj Baiyda <deeprajbaidya@gmail.com>
2023-04-15 18:34:03 +0530

    fix: API docs for NeuSearchBar

--- lib/src/widgets/containers/neu_search_bar.dart
+++ lib/src/widgets/containers/neu_search_bar.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:neubrutalism_ui/neubrutalism_ui.dart';
 
 class NeuSearchBar extends StatefulWidget {
+  /// A customizable neubrutalist-style Search Bar.
+  ///
+  /// The Search-Bar has customizable keyboardType,inputStyle,hintText,
+  /// intStyle,searchBarHeight,searchBarWidth,The button's behavior is specified with an `onPressed`
+  /// callback function.
+  ///
+  /// This button is built using Flutter's `Material` widget, and is designed to
+  /// follow the Neubrutalism UI style guidelines.
+  ///
+  /// *[Constants]
+
+  ///   - const neuBlack = Colors.black;
+  ///   - const neuDefault1 = Colors.teal;
+  ///   - const neuShadow = Color(0xFF080808);
+  ///
+  ///   - const neuBorder = 3.0;
+  ///   - const neuShadowBlurRadius = 0.0;
+  ///
+  ///   - const neuOffset = Offset(4, 4);
+  ///   - const neuBlurStyle = BlurStyle.solid;
   const NeuSearchBar({
     Key? key,
     this.keyboardType,
@@ -22,21 +42,93 @@ class NeuSearchBar extends StatefulWidget {
     this.shadowColor = neuShadow,
   }) : super(key: key);
 
+  /// - keyboardType (optional): A TextInputType that defines the type of input expected from the user.
+  ///
+  ///  By default, it is set to null.
   final TextInputType? keyboardType;
 
+  /// - inputStyle (optional): A TextStyle that defines the style of the text entered in the search bar.
+  ///
+  /// By default, it is set to null.
+
   final TextStyle? inputStyle;
+
+  /// - hintText (optional): A String that displays a hint for the user in the search bar.
+  ///
+  /// By default, it is set to null.
+
   final String? hintText;
+
+  /// - searchController (optional): A TextEditingController that controls the text being entered in the search bar.
+  ///
+  /// By default, it is set to null.
+
   final TextEditingController? searchController;
+
+  /// - hintStyle (optional): A TextStyle that defines the style of the hint text displayed in the search bar.
+  ///
+  /// By default, it is set to null.
+
   final TextStyle? hintStyle;
+
+  /// - searchBarHeight (optional): A double that defines the height of the search bar.
+  ///
+  /// By default, it is set to null.
+
   final double? searchBarHeight;
+
+  /// - searchBarWidth (optional): A double that defines the width of the search bar.
+  ///
+  /// By default, it is set to null.
+
   final double? searchBarWidth;
+
+  /// - borderWidth (optional): A double that defines the width of the border of the search bar.
+  ///
+  /// By default, it is set to 1.
+
   final double borderWidth;
+
+  /// - shadowBlurRadius (optional): A double that defines the blur radius of the shadow of the search bar.
+  ///
+  /// By default, it is set to 4.
+
   final double shadowBlurRadius;
+
+  /// - searchBarIcon (optional): An Icon widget that displays an icon in the search bar.
+  ///
+  /// By default, it is set to null.
+
   final Icon? searchBarIcon;
+
+  /// - shadowOffset (optional): An Offset that defines the offset of the shadow of the search bar.
+  ///
+  /// By default, it is set to (0, 0).
+
   final Offset shadowOffset;
+
+  /// - borderRadius (optional): A BorderRadiusGeometry that defines the border radius of the search bar.
+  ///
+  /// By default, it is set to null.
+
   final BorderRadiusGeometry? borderRadius;
+
+  /// - borderColor (optional): A Color that defines the color of the border of the search bar.
+  ///
+  /// By default, it is set to neuBlack.
+
   final Color borderColor;
+
+  /// - searchBarColor (optional): A Color that defines the color of the search bar.
+  ///
+  /// By default, it is set to null.
+
   final Color? searchBarColor;
+
+  /// - shadowColor (optional): A Color that defines the color of the shadow of the search bar.
+  ///
+  /// By default, it is set to neuShadow.
+
   final Color shadowColor;
 
   @override
