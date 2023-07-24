afb627a21290be33a53ab25720811fad58be63de
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-07-24 22:07:43 +0530

    🧹 CHORE: component export

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -4,3 +4,5 @@ export './buttons/neu_icon_button.dart';
 export './containers/neu_card.dart';
 export 'containers/neu_search_bar.dart';
 export 'containers/neu_container.dart';
+
+export './bottom nav/neu_bottom_nav.dart';
